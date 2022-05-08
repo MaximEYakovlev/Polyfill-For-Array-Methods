edef89126fbe9a60da081e9d54f727052458358a
Maxim Yakovlev <it.trainee@yandex.ru>
2022-05-08 06:17:33 +0300

    added a polyfill for the array method .map()

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = function () { }
+if (!Array.prototype.map) {
+    Array.prototype.map = function (callback) {
+        const arr = []
+        for (let i = 0; i < this.length; i++) {
+            arr.push(callback(this[i], i, this))
+        }
+        return arr
+    }
+}
+
+
+
+
+
