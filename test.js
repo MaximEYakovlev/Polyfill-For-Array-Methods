fb1a35910f083d098004f04aeea729ee5a569d76
Maxim Yakovlev <it.trainee@yandex.ru>
2022-05-12 23:27:59 +0300

    added testing of the array method .map() (not completed)

---
+++ test.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect
+
+const polyfills = require('./index')
+
+describe('Testing polyfills', () => {
+    before(() => {
+        polyfills()
+    })
+
+    describe('Testing map', () => {
+        it('Check — wrong type', () => {
+            function badFn() {
+                Array.prototype.map.call(true, (value) => value)
+            }
+            expect(badFn).to.throw(TypeError)
+        })
+
+        it('Check — no callback', () => {
+            function badFn() {
+                [1, 2, 3].map()
+            }
+            expect(badFn).to.throw(TypeError)
+        })
+    })
+})
